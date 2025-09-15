3665fb1b55c1af85140e0411705e3bf425c2db41
sfe dd <scottierieh2@gmail.com>
2025-09-15 05:02:48 +0000

    covariates에 맞는 변수열 데이터로 교체가능?

--- src/lib/example-datasets/survival-data.ts
+++ src/lib/example-datasets/survival-data.ts
@@ -1,24 +1,23 @@
 
-export const survivalData = `tenure,churn,group
-7,1,A
-4,1,B
-8,0,A
-12,1,B
-24,0,A
-2,1,A
-31,0,B
-10,1,A
-15,0,B
-1,1,A
-6,1,B
-18,0,A
-22,1,B
-3,1,A
-28,0,B
-5,1,A
-35,0,B
-9,1,A
-13,0,B
-2,1,A
+export const survivalData = `tenure,churn,group,age,plan_type
+7,1,A,55,Basic
+4,1,B,42,Premium
+8,0,A,65,Basic
+12,1,B,38,Premium
+24,0,A,70,Premium
+2,1,A,25,Basic
+31,0,B,58,Premium
+10,1,A,45,Basic
+15,0,B,51,Premium
+1,1,A,33,Basic
+6,1,B,48,Basic
+18,0,A,61,Premium
+22,1,B,39,Premium
+3,1,A,28,Basic
+28,0,B,66,Basic
+5,1,A,31,Premium
+35,0,B,72,Basic
+9,1,A,41,Premium
+13,0,B,56,Basic
+2,1,A,29,Premium
 `;
-
