861f78105d16d0680492754eb324a8666a5a4461
sfe dd <scottierieh2@gmail.com>
2025-09-16 15:30:59 +0000

    아니 repeated anova 예제 다시 만들어저ㅜ

---
+++ src/lib/example-datasets/rm-anova-data.ts
@@ -0,0 +1,23 @@
+
+export const rmAnovaData = `athlete_id,group,time1,time2,time3
+S01,Control,55,58,57
+S02,Control,60,62,61
+S03,Control,62,65,66
+S04,Control,58,60,61
+S05,Control,59,61,60
+S06,Control,61,63,64
+S07,Control,57,59,58
+S08,Control,63,66,65
+S09,Control,54,56,55
+S10,Control,56,57,58
+S11,Treatment,65,72,78
+S12,Treatment,68,75,82
+S13,Treatment,66,74,80
+S14,Treatment,70,78,85
+S15,Treatment,64,71,77
+S16,Treatment,71,79,86
+S17,Treatment,69,76,83
+S18,Treatment,67,73,81
+S19,Treatment,72,80,88
+S20,Treatment,68,75,84
+`;
