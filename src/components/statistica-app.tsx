e4e1b9a51f23e3cb2161dd08003bb772245d402b
sfe dd <scottierieh2@gmail.com>
2025-09-20 12:55:16 +0000

    안보이는데?

--- src/components/statistica-app.tsx
+++ src/components/statistica-app.tsx
@@ -60,6 +60,7 @@ import {
   Share2,
   GitCommit,
   ClipboardList,
+  Search,
 } from 'lucide-react';
 import * as XLSX from 'xlsx';
 import {
@@ -128,7 +129,7 @@ import TTestPage from './pages/t-test-page';
 import AhpPage from './pages/ahp-page';
 import DidPage from './pages/did-page';
 import DelphiPage from './pages/delphi-page';
-import Link from 'next/link';
+import SurveyApp from './survey-app';
 
 
 type AnalysisType = 'stats' | 'correlation' | 'partial-correlation' | 'one-way-anova' | 'two-way-anova' | 'ancova' | 'manova' | 'mancova' | 'reliability' | 'visuals' | 'discriminant' | 'efa' | 'cfa' | 'mediation' | 'moderation' | 'nonparametric' | 'hca' | 't-test' | 'regression' | 'logistic-regression' | 'glm' | 'kmeans' | 'kmedoids' | 'hdbscan' | 'frequency' | 'crosstab' | 'sem' | 'conjoint' | 'cbc' | 'ipa' | 'pca' | 'survival' | 'wordcloud' | 'gbm' | 'sentiment' | 'meta-analysis' | 'mds' | 'rm-anova' | 'dbscan' | 'nonlinear-regression' | 'sna' | 'topic-modeling' | 'dea' | 'ahp' | 'did' | 'delphi' | 'survey' | string;
@@ -197,6 +198,7 @@ const analysisPages: Record<string, React.ComponentType<any>> = {
     ahp: AhpPage,
     did: DidPage,
     delphi: DelphiPage,
+    survey: SurveyApp,
 };
 
 const analysisMenu = [
@@ -623,7 +625,7 @@ export default function StatisticaApp() {
                               <SidebarMenuButton
                                   onClick={() => {
                                       if (method.id === 'survey') {
-                                        window.open('/dashboard/survey?id=1', '_blank');
+                                        setActiveAnalysis(method.id as AnalysisType);
                                       } else {
                                         setActiveAnalysis(method.id as AnalysisType)
                                       }
@@ -687,7 +689,7 @@ export default function StatisticaApp() {
                 <div />
             </header>
             
-            {hasData && activeAnalysis !== 'stats' && activeAnalysis !== 'wordcloud' && activeAnalysis !== 'sentiment' && activeAnalysis !== 'meta-analysis' && activeAnalysis !== 'ahp' && activeAnalysis !== 'delphi' &&(
+            {hasData && activeAnalysis !== 'stats' && activeAnalysis !== 'wordcloud' && activeAnalysis !== 'sentiment' && activeAnalysis !== 'meta-analysis' && activeAnalysis !== 'ahp' && activeAnalysis !== 'delphi' && activeAnalysis !== 'survey' &&(
               <DataPreview 
                 fileName={fileName}
                 data={data}
