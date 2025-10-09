7e0a80484865951c3bff9a0ffb482864c5f69508
sfe dd <scottierieh2@gmail.com>
2025-10-09 15:23:17 +0000

    createsurvey에도 추가해야지

--- src/app/dashboard/createsurvey/page.tsx
+++ src/app/dashboard/createsurvey/page.tsx
@@ -12,7 +12,7 @@ import SurveyStylePanel from '@/components/survey/SurveyStylePanel';
 import { QuestionTypePalette } from '@/components/survey/QuestionTypePalette';
 import { SpecialAnalysisPalette } from '@/components/survey/SpecialAnalysisPalette';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
-import { choiceBasedConjointTemplate, ratingBasedConjointTemplate, ipaTemplate, vanWestendorpTemplate, turfTemplate, gaborGrangerTemplate1, gaborGrangerTemplate2, ahpCriteriaOnlyTemplate, ahpWithAlternativesTemplate, csatTemplate, semanticDifferentialTemplate, brandFunnelTemplate } from '@/lib/survey-templates';
+import { choiceBasedConjointTemplate, ratingBasedConjointTemplate, ipaTemplate, vanWestendorpTemplate, turfTemplate, gaborGrangerTemplate1, gaborGrangerTemplate2, ahpCriteriaOnlyTemplate, ahpWithAlternativesTemplate, csatTemplate, semanticDifferentialTemplate, brandFunnelTemplate, servqualTemplate } from '@/lib/survey-templates';
 import { Tabs, TabsTrigger, TabsContent, TabsList } from '@/components/ui/tabs';
 import SurveyView from '@/components/survey-view';
 
@@ -90,7 +90,10 @@ export default function CreateSurveyPage() {
                 break;
             case 'brand-funnel':
                 selectedTemplate = brandFunnelTemplate;
-                break;    
+                break;
+            case 'servqual':
+                selectedTemplate = servqualTemplate;
+                break;
             default:
                 return;
         }
