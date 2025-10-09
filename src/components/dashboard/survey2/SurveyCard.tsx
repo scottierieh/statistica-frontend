6fb167d01df9250fbb5bb01b70a1430190f415a8
sfe dd <scottierieh2@gmail.com>
2025-10-09 09:25:42 +0000

    Unhandled Runtime Error
    ReferenceError: AnimatePresence is not defined

--- src/components/dashboard/survey2/SurveyCard.tsx
+++ src/components/dashboard/survey2/SurveyCard.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import Link from 'next/link';
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
