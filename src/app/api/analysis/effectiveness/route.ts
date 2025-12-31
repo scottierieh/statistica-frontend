95c73bdf3852b2b5f0442116583bc05309fc1a4a
sfe dd <scottierieh2@gmail.com>
2025-12-31 13:18:11 +0000

    Build Error
    Module not found: Can't resolve '../../../proxy'
    
    ./src/app

--- src/app/api/analysis/effectiveness/route.ts
+++ src/app/api/analysis/effectiveness/route.ts
@@ -1,6 +1,6 @@
 
 import { NextRequest } from 'next/server';
-import { proxyToPython } from '../../../proxy';
+import { proxyToPython } from '../../proxy';
 
 const PYTHON_API_URL = process.env.PYTHON_API_URL || 'http://127.0.0.1:8000';
 
